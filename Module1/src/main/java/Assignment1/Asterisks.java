0275752a2c0f50bdc95f21db5066914222e32e5d
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-20 09:46:38 +0300

    Finish 1.1 Java as a programming language

---
+++ Module1/src/main/java/Assignment1/Asterisks.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+//   *
+//  ***
+// *****
+//*******
+
+public class Asterisks {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
+
---
+++ Module1/src/main/java/Assignment1/Cat.java
@@ -0,0 +1,30 @@
+package Assignment1;
+
+//The cat named Whiskers says: Meow!
+//The cat named Whiskers says: Meow!
+//The cat named Rex says: Meow!
+//The cat named Whiskers says: Meow!
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println("The cat named " + name + " says: Meow!");
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Cat class with a name
+        Cat catWhiskers = new Cat("Whiskers");
+        Cat catRex = new Cat("Rex");
+
+        // Call the meow method on the cat instance
+        catWhiskers.meow();
+        catWhiskers.meow();
+        catRex.meow();
+        catWhiskers.meow();
+    }
+}
