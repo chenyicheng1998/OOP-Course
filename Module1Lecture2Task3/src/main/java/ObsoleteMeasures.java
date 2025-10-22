183d310b60cece9ff2acfca5cd39a17548d13eb2
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-22 09:46:30 +0300

    Finish Module1 Lecture2

--- Module1Lecture2Task3/src/main/java/ObsoleteMeasures.java
+++ Module1Lecture2Task3/src/main/java/ObsoleteMeasures.java
@@ -6,23 +6,22 @@ public class ObsoleteMeasures {
 
         final double GRAMS_PER_LUOTI = 13.28;
         final int LUOTI_PER_NAULA = 32;
-        final int NAULA_PER_LEIVISKA = 20;
+        final int NAULA_PER_LEIVISKÄ = 20;
 
-        // 计算一个 leiviskä 等于多少 luoti
-        final int LUOTI_PER_LEIVISKA = NAULA_PER_LEIVISKA * LUOTI_PER_NAULA; // 20 * 32 = 640
+        final int LUOTI_PER_LEIVISKÄ = NAULA_PER_LEIVISKÄ * LUOTI_PER_NAULA;
 
         System.out.print("Weight (g): ");
         double totalGrams = Double.parseDouble(scanner.nextLine());
 
         double totalLuoti = totalGrams / GRAMS_PER_LUOTI;
 
-        int leiviska = (int) (totalLuoti / LUOTI_PER_LEIVISKA);
+        int leiviska = (int) (totalLuoti / LUOTI_PER_LEIVISKÄ);
 
-        double remainingLuotiAfterLeiviska = totalLuoti % LUOTI_PER_LEIVISKA;
+        double remainingLuotiAfterLeiviskä = totalLuoti % LUOTI_PER_LEIVISKÄ;
 
-        int naula = (int) (remainingLuotiAfterLeiviska / LUOTI_PER_NAULA);
+        int naula = (int) (remainingLuotiAfterLeiviskä / LUOTI_PER_NAULA);
 
-        double remainingLuoti = remainingLuotiAfterLeiviska % LUOTI_PER_NAULA;
+        double remainingLuoti = remainingLuotiAfterLeiviskä % LUOTI_PER_NAULA;
 
         System.out.printf("%.0f grams is %d leiviskä, %d naula, and %.2f luoti.\n",
                 totalGrams, leiviska, naula, remainingLuoti);
